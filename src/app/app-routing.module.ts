9dc77161f522ffefc71fbc427bbaf3b46c468ba6
Joanna Chądzyńska <jmalek87@gmail.com>
2021-10-17 15:34:52 +0200

    fix routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { RegionDetailsComponent } from './region-details/region-details.component';
-import { RegionsComponent } from './regions/regions.component';
+import { RegionsComponent } from './world/regions/regions.component';
 
 const routes: Routes = [
   { path: '', component: RegionsComponent },
@@ -12,7 +12,6 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AboutComponent } from './about/about.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
 import { RegionDetailsComponent } from './region-details/region-details.component';
-import { RegionsComponent } from './regions/regions.component';
+import { UiModule } from './ui/ui.module';
+import { WorldModule } from './world/world.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    RegionsComponent,
-    AboutComponent,
-    FooterComponent,
-    HeaderComponent,
-    RegionDetailsComponent,
+  declarations: [AppComponent, AboutComponent, RegionDetailsComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    WorldModule,
+    UiModule,
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+
   providers: [],
   bootstrap: [AppComponent],
 })
