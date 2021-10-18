ae32561ea09278855e96076103b700f49f992e5a
Joanna Chądzyńska <jchadzynska@gwo.pl>
2021-10-18 11:16:03 +0200

    upate world module: declare and export CountryComponent, declare RegionComponent, import SharedModule

--- src/app/world/world.module.ts
+++ src/app/world/world.module.ts
@@ -1,11 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { SharedModule } from './../shared/shared.module';
+import { CountryComponent } from './country/country.component';
+import { RegionComponent } from './regions/region/region.component';
 import { RegionsComponent } from './regions/regions.component';
 import { WorldRoutingModule } from './world-routing.module';
 
 @NgModule({
-  declarations: [RegionsComponent],
-  imports: [CommonModule, WorldRoutingModule],
-  exports: [RegionsComponent],
+  declarations: [RegionsComponent, RegionComponent, CountryComponent],
+  imports: [CommonModule, WorldRoutingModule, SharedModule],
+  exports: [RegionsComponent, CountryComponent],
 })
 export class WorldModule {}
