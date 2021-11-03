60c7a6d6d3a8fe3c66e46783e2c6364ccbee2896
Joanna Chądzyńska <jmalek87@gmail.com>
2021-11-03 21:36:35 +0100

    update components

--- src/app/world/country/country.component.ts
+++ src/app/world/country/country.component.ts
@@ -6,36 +6,36 @@ import { Country } from 'src/app/country';
 import * as countriesSelectors from './../store/selectors';
 
 @Component({
-    selector: 'app-country',
-    templateUrl: './country.component.html',
-    styleUrls: ['./country.component.scss'],
+  selector: 'app-country',
+  templateUrl: './country.component.html',
+  styleUrls: ['./country.component.scss'],
 })
 export class CountryComponent implements OnInit {
-    country!: Country;
-    countryName: string = '';
-    borders$!: any;
+  country!: Country;
+  countryName: string = '';
+  borders$!: any;
 
-    constructor(
-        private route: ActivatedRoute,
-        private location: Location,
-        private store: Store<any>
-    ) {}
+  constructor(
+    private route: ActivatedRoute,
+    private location: Location,
+    private store: Store<any>
+  ) {}
 
-    ngOnInit(): void {
-        const name = this.route.snapshot.paramMap.get('country')!;
-        this.countryName = name;
-        this.route.data.subscribe((data) => {
-            this.country = data.country[0];
-        });
+  ngOnInit(): void {
+    const name = this.route.snapshot.paramMap.get('country')!;
+    this.countryName = name;
+    this.route.data.subscribe((data) => {
+      this.country = data.country[0];
+    });
 
-        this.country.borders.forEach((border) => {
-            const b = this.store.select(
-                countriesSelectors.selectCountriesByCioc(border)
-            );
-        });
-    }
+    this.country.borders.forEach((border) => {
+      const b = this.store.select(
+        countriesSelectors.selectCountriesByCioc(border)
+      );
+    });
+  }
 
-    goBack(): void {
-        this.location.back();
-    }
+  goBack(): void {
+    this.location.back();
+  }
 }
