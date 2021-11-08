c14594322ec85ddc4c15d69e18afd9d4b69efeab
Joanna Chądzyńska <jchadzynska@gwo.pl>
2021-11-08 12:45:59 +0100

    add modal

--- src/app/ui/components/about-modal/about-modal.component.ts
+++ src/app/ui/components/about-modal/about-modal.component.ts
@@ -4,6 +4,7 @@ import {
   EventEmitter,
   Output,
 } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-about-modal',
@@ -12,7 +13,16 @@ import {
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AboutModalComponent {
+  dangerResourceUrl = 'https://github.com/joannachadzynska';
+  trustedResourceUrl: SafeResourceUrl;
+
   @Output() closeClick = new EventEmitter<void>();
+
+  constructor(private sanitizer: DomSanitizer) {
+    this.trustedResourceUrl = this.sanitizer.bypassSecurityTrustResourceUrl(
+      this.dangerResourceUrl
+    );
+  }
 
   onClose(): void {
     this.closeClick.emit();
